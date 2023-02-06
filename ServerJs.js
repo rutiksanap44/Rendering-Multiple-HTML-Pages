ab87eee3296789ed3b6b683c96613800dddd2973
rutiksanap44 <99959986+rutiksanap44@users.noreply.github.com>
2023-02-06 15:21:06 +0530

    Successfully Rendered

--- ServerJs.js
+++ ServerJs.js
@@ -1,1 +1,42 @@
 const http = require('http');
+const fs = require('fs');
+const port = 6223;
+
+function requestHandler(req,res){
+    console.log(req.url);
+
+    res.writeHead(200, {'Content-type': 'text-html'});
+
+    let filePath;
+
+    switch(req.url){
+        case '/':
+            filePath = './Main.html';
+            break;
+        case '/profile':
+            filePath = './Profile.html'
+            break;
+        default:
+            filePath = './402.html';
+    }
+    
+    fs.readFile(filePath,function(err,data){
+        if(err){
+            console.log(`Error occured ${err}`);
+            return res.end('<h1>Error Occured!</h1>');
+        }
+        return res.end(data);
+    })
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(port,function(err){
+    if(err){
+        console.log(err);
+        return;
+    }
+    else{
+        console.log(`Server is up and runnig on port no : ${port}`);
+    }
+});
